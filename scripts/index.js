3688adf0989105061d2b99c56fc4e2a2721166a3
manuelgutierrez8 <36770340+manuelgutierrez8@users.noreply.github.com>
2019-09-12 23:52:18 -0600

    Initial files

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+/**
+ * index.js
+ * - All our useful JS goes here, awesome!
+ */
+/*$(".jumbotron").css({ height: $(window).height() + "px" });
+
+$(window).on("resize", function() {
+  $(".jumbotron").css({ height: $(window).height() + "px" });
+});*/
