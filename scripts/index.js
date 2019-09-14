1ea975e672746d0f5d2fe7a467b1d657483110f3
Manuel <manuel.gutierrezrojas8@gmail.com>
2019-09-14 12:04:41 -0600

    Using real data to display information

--- scripts/index.js
+++ scripts/index.js
@@ -9,35 +9,82 @@ $(window).on("resize", function() {
 });*/
 
 var lang = "en";
+var redirectPage = "https://www.google.com";
+var userData;
 
 $(window).on("load", function() {
-  translateForm();
+  //Get query strings
+  var urlParams = new URLSearchParams(window.location.search);
+  let guestId = "";
+
+  if (urlParams.has("guest")) {
+    guestId = urlParams.get("guest");
+    getData(guestId);
+  } else {
+    window.location.href = redirectPage;
+  }
 });
+
+$(document).on("ready", function() {
+  setUserInformation();
+});
+
+function getData(guestId) {
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      let db = JSON.parse(this.responseText);
+
+      userData = db.find(function(el) {
+        return el.id === guestId;
+      });
+
+      if (!userData) {
+        window.location.href = redirectPage;
+      } else {
+        setUserInformation();
+      }
+    }
+  };
+  xmlhttp.open("GET", "db.json", true);
+  xmlhttp.send();
+}
+
+function setUserInformation() {
+  lang = userData.language;
+
+  document.querySelector(".js-name").textContent = userData.name;
+  document.querySelector(".js-guests").textContent = userData.guests;
+
+  translateForm();
+}
 
 function getDictionary() {
   return {
-    "es": {
-      "intro": "Tienen el agrado de invitarle a la celebración de su boda",
-      "date": "Viernes 01 de noviembre",
-      "mass": "Misa", 
-      "mass-place": "Capilla de nuestra señora del sagrado Colegio Centroamérica",
-      "event": "Recepción", 
+    es: {
+      intro: "Tienen el agrado de invitarle a la celebración de su boda",
+      date: "Viernes 01 de noviembre",
+      mass: "Misa",
+      "mass-place":
+        "Capilla de nuestra señora del sagrado Colegio Centroamérica",
+      event: "Recepción",
       "event-place": "Casa & jardín Eventos - Las Colinas",
-      "rsvp": "Por favor RSVP antes del 20 de octubre",
-      "guests": " Invitado(s)",
-      "message": "Lluvia de sobres",
+      rsvp: "Por favor RSVP antes del 20 de octubre",
+      guests: " Invitado(s)",
+      message: "Lluvia de sobres",
       "button-text": "Asistiremos"
     },
-    "en": {
-      "intro": "Request the honor of your presence at the ceremony of their wedding",
-      "date": "November 1st, 2019",
-      "mass": "Ceremony", 
+    en: {
+      intro:
+        "Request the honor of your presence at the ceremony of their wedding",
+      date: "November 1st, 2019",
+      mass: "Ceremony",
       "mass-place": "Capel Nuestra Señora del Sagrado Colegio Centroamerica",
-      "event": "Reception", 
+      event: "Reception",
       "event-place": "Casa & jardín Eventos - Las Colinas",
-      "rsvp": "Kindly RSVP by October 20th.",
-      "guests": " Guest(s)",
-      "message": "Cards Box",
+      rsvp: "Kindly RSVP by October 20th.",
+      guests: " Guest(s)",
+      message: "Cards Box",
       "button-text": "Joyfully Accepts"
     }
   };
@@ -46,7 +93,7 @@ function getDictionary() {
 function translateForm() {
   let dictionary = getDictionary();
   let phrases = dictionary[lang];
-  let sectionPhrases = Object.keys(phrases)
+  let sectionPhrases = Object.keys(phrases);
 
   sectionPhrases.forEach(prop => {
     var elements = document.querySelectorAll(`[data-locale-text="${prop}"]`);
