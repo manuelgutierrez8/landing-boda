1abe69ffaad2ba643ebbcdaf576efe3a1f2bff85
Manuel <manuel.gutierrezrojas8@gmail.com>
2019-09-15 01:20:37 -0600

    adding thank you page, using new api

--- scripts/index.js
+++ scripts/index.js
@@ -7,15 +7,14 @@
 $(window).on("resize", function() {
   $(".jumbotron").css({ height: $(window).height() + "px" });
 });*/
-
 var lang = "en";
 var redirectPage = "default.html";
 var userData;
+var guestId = "";
 
 $(window).on("load", function() {
   //Get query strings
   var urlParams = new URLSearchParams(window.location.search);
-  let guestId = "";
 
   if (urlParams.has("guest")) {
     guestId = urlParams.get("guest");
@@ -46,8 +45,49 @@ function getData(guestId) {
       }
     }
   };
-  xmlhttp.open("GET", "db.json", true);
+  xmlhttp.open(
+    "GET",
+    "https://jsonblob.com/api/8c48abb9-d77c-11e9-93e2-6f9a6af437fd",
+    true
+  );
   xmlhttp.send();
+}
+
+function confirmAssistance() {
+  let http = new XMLHttpRequest();
+  http.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      let actualdb = JSON.parse(this.responseText);
+
+      finalUserData = actualdb.find(function(el) {
+        return el.id === guestId;
+      });
+
+      finalUserData.status = "confirmed";
+
+      updateDatabase(actualdb);
+    }
+  };
+  http.open(
+    "GET",
+    "https://jsonblob.com/api/8c48abb9-d77c-11e9-93e2-6f9a6af437fd",
+    true
+  );
+  http.send();
+}
+
+function updateDatabase(database) {
+  $.ajax({
+    url: "https://jsonblob.com/api/8c48abb9-d77c-11e9-93e2-6f9a6af437fd",
+    type: "PUT",
+    data: JSON.stringify(database),
+    contentType: "application/json",
+    dataType: "json",
+    success: function(data, textStatus, jqXHR) {
+      var json = JSON.stringify(data);
+      console.log(json)
+    }
+  });
 }
 
 function setUserInformation() {
@@ -72,8 +112,8 @@ function getDictionary() {
       rsvp: "Por favor RSVP antes del 20 de octubre",
       guests: "Total de invitados: ",
       message: "Lluvia de sobres",
-      "button-text": "Asistiremos", 
-      "mass-hour": "5:00 PM",      
+      "button-text": "Asistiremos",
+      "mass-hour": "5:00 PM",
       "event-hour": "7:00 PM"
     },
     en: {
@@ -81,14 +121,15 @@ function getDictionary() {
         "Request the honor of your presence at the ceremony of their wedding",
       date: "November 1st, 2019",
       mass: "Ceremony",
-      "mass-place": "Capel Nuestra Señora del Sagrado Corazón - Colegio Centroamerica",
+      "mass-place":
+        "Capel Nuestra Señora del Sagrado Corazón - Colegio Centroamerica",
       event: "Reception",
       "event-place": "Casa & Jardín Eventos - Las Colinas",
       rsvp: "Kindly RSVP by October 20th.",
       guests: "Total Guests: ",
       message: "Cards Box",
       "button-text": "Joyfully Accepts",
-      "mass-hour": "5:00 PM",      
+      "mass-hour": "5:00 PM",
       "event-hour": "7:00 PM"
     }
   };
